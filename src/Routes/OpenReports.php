20721e99b6e9955bbc0763272e8dcd5e81a5f491
Thomas Hoffmann <t.hoffmann.1979@googlemail.com>
2025-03-07 12:53:24 +0100

    all

--- src/Routes/OpenReports.php
+++ src/Routes/OpenReports.php
@@ -1,33 +1,37 @@
 <?php
+
 namespace Tualo\Office\FinTS\Routes;
+
 use Tualo\Office\Basic\TualoApplication as A;
 use Tualo\Office\Basic\Route;
 use Tualo\Office\Basic\IRoute;
 
 
-class OpenReports implements IRoute{
+class OpenReports implements IRoute
+{
 
-    public static function columns_table_name_filter($array,$table_name) {
-          $res = array();
-          foreach ($array as $value) {
-            if ($value['table_name']==$table_name){
-              $res[] = ' replace( replace( ifnull( '.$value['table_name'].'.'.$value['column_name'].',\'\'),\'\\\'\',\' \') ,\'"\',\' \')  ';
+    public static function columns_table_name_filter($array, $table_name)
+    {
+        $res = array();
+        foreach ($array as $value) {
+            if ($value['table_name'] == $table_name) {
+                $res[] = ' replace( replace( ifnull( ' . $value['table_name'] . '.' . $value['column_name'] . ',\'\'),\'\\\'\',\' \') ,\'"\',\' \')  ';
             }
-          };
-          return $res;
-       
-        }
-    public static function register(){
-        Route::add('/fints/openreports',function($matches){
+        };
+        return $res;
+    }
+    public static function register()
+    {
+        Route::add('/fints/openreports', function ($matches) {
 
             $db = A::get('session')->getDB();
-            try{
-                
+            try {
+
                 $sql = 'select id,name,tabellenzusatz,adress_bezug,preis_bezug,bezug_id,bezug_kst from blg_config';
-                $belege=$db->direct($sql,[],'id');
+                $belege = $db->direct($sql, [], 'id');
 
                 $sql = 'select id,name,lower(tabellenzusatz) tabellenzusatz,adress_bezug,preis_bezug,bw_faktor,bezug_id,bezug_kst from blg_config';
-                $belege_tz = $db->direct($sql,[],'tabellenzusatz');
+                $belege_tz = $db->direct($sql, [], 'tabellenzusatz');
 
 
                 /*
@@ -36,9 +40,9 @@ class OpenReports implements IRoute{
                 */
 
                 $sql = 'select table_name,displayfield,searchfield from ds';
-                $ds_config = $db->direct($sql,[],'table_name');
+                $ds_config = $db->direct($sql, [], 'table_name');
 
-                $tzlist = ['rechnung','krechnung'];
+                $tzlist = ['rechnung', 'krechnung'];
                 // array('rechnung','krechnung');
                 $columns = $db->direct('select table_name,column_name from ds_column where  column_name<>"offen" ');
 
@@ -72,19 +76,18 @@ class OpenReports implements IRoute{
 
                 ';
                 $formula = array();
-                if (isset($_REQUEST['all'])&&($_REQUEST['all']==1)){
-                    if (isset($_REQUEST['query'])){
-                    $words = explode(' ',$_REQUEST['query']);
-                    foreach ($words as $word) {
-                        $formula[] = ' if({dstable}.{searchfield} like \'%'.$word.'%\',1,0 ) ';
-                        $formula[] = ' if({dstable}.{idcolumn} like \'%'.$word.'%\',1,0 ) ';
-                        $formula[] = ' if(blg_hdr_{tz}.id like \'%'.$word.'%\',1,0 ) ';
-                        $formula[] = ' if(blg_hdr_{tz}.referenz like \'%'.$word.'%\',1,0 ) ';
+                if (isset($_REQUEST['all']) && ($_REQUEST['all'] == 1)) {
+                    if (isset($_REQUEST['query'])) {
+                        $words = explode(' ', $_REQUEST['query']);
+                        foreach ($words as $word) {
+                            $formula[] = ' if({dstable}.{searchfield} like \'%' . $word . '%\',1,0 ) ';
+                            $formula[] = ' if({dstable}.{idcolumn} like \'%' . $word . '%\',1,0 ) ';
+                            $formula[] = ' if(blg_hdr_{tz}.id like \'%' . $word . '%\',1,0 ) ';
+                            $formula[] = ' if(blg_hdr_{tz}.referenz like \'%' . $word . '%\',1,0 ) ';
+                        }
+                        $sql_template .= ' WHERE {searchformula_where} >= ' . count($words) . '  ';
                     }
-                    $sql_template .= ' WHERE {searchformula_where} >= '.count($words).'  ';
-
-                    }
-                }else{
+                } else {
                     $sql_template .= ' WHERE round(blg_hdr_{tz}.offen,1) <> 0 ';
                 }
                 $sqls = array();
@@ -93,38 +96,38 @@ class OpenReports implements IRoute{
                     $bw_faktor = $belege_tz[$tz]['bw_faktor'];
                     $tsql = $sql_template;
 
-                    if (count($formula)>0){
-                    $tsql = str_replace('{searchformula}', ' '.implode('+',$formula).' searchformula, ',$tsql);
-                    $tsql = str_replace('{searchformula_where}', ' '.implode('+',$formula).'  ',$tsql);
-                    }else{
-                    $tsql = str_replace('{searchformula}', ' 0 searchformula,',$tsql);
-                    $tsql = str_replace('{searchformula_where}', ' 0 ',$tsql);
+                    if (count($formula) > 0) {
+                        $tsql = str_replace('{searchformula}', ' ' . implode('+', $formula) . ' searchformula, ', $tsql);
+                        $tsql = str_replace('{searchformula_where}', ' ' . implode('+', $formula) . '  ', $tsql);
+                    } else {
+                        $tsql = str_replace('{searchformula}', ' 0 searchformula,', $tsql);
+                        $tsql = str_replace('{searchformula_where}', ' 0 ', $tsql);
                     }
 
 
-                    $tsql = str_replace('{kst_concat}', ' and {dstable}.kostenstelle = blg_{bez}_{tz}.kostenstelle ',$tsql);
-                    $tsql = str_replace('{tz}', $tz,$tsql);
-                    $tsql = str_replace('{bw_faktor}', $bw_faktor,$tsql);
-                    $tsql = str_replace('{belegartid}', $beleg_index,$tsql);
+                    $tsql = str_replace('{kst_concat}', ' and {dstable}.kostenstelle = blg_{bez}_{tz}.kostenstelle ', $tsql);
+                    $tsql = str_replace('{tz}', $tz, $tsql);
+                    $tsql = str_replace('{bw_faktor}', $bw_faktor, $tsql);
+                    $tsql = str_replace('{belegartid}', $beleg_index, $tsql);
 
-                    $tsql = str_replace('{bez}', $belege_tz[$tz]['adress_bezug'] ,$tsql);
-                    $tsql = str_replace('{dstable}', $belege_tz[$tz]['adress_bezug'],$tsql);
-                    $tsql = str_replace('{idcolumn}', $belege_tz[$tz]['bezug_id']  ,$tsql);
-                    $tsql = str_replace('{dsname}', $ds_config[ $belege_tz[$tz]['adress_bezug'] ]['displayfield'],$tsql);
-                    $tsql = str_replace('{searchfield}', $ds_config[ $belege_tz[$tz]['adress_bezug'] ]['searchfield'],$tsql);
+                    $tsql = str_replace('{bez}', $belege_tz[$tz]['adress_bezug'], $tsql);
+                    $tsql = str_replace('{dstable}', $belege_tz[$tz]['adress_bezug'], $tsql);
+                    $tsql = str_replace('{idcolumn}', $belege_tz[$tz]['bezug_id'], $tsql);
+                    $tsql = str_replace('{dsname}', $ds_config[$belege_tz[$tz]['adress_bezug']]['displayfield'], $tsql);
+                    $tsql = str_replace('{searchfield}', $ds_config[$belege_tz[$tz]['adress_bezug']]['searchfield'], $tsql);
 
 
-                    $matchcols = self::columns_table_name_filter($columns,$belege_tz[$tz]['adress_bezug']);
-                    if (count($matchcols)>0){
-                    $tsql = str_replace('{matchcols}', ' concat( \'["\','.implode(',\'","\',',$matchcols).' ,\'"]\') matchcols, ',$tsql);
-                    }else{
-                    $tsql = str_replace('{matchcols}', ' \'[]\' matchcols,',$tsql);
+                    $matchcols = self::columns_table_name_filter($columns, $belege_tz[$tz]['adress_bezug']);
+                    if (count($matchcols) > 0) {
+                        $tsql = str_replace('{matchcols}', ' concat( \'["\',' . implode(',\'","\',', $matchcols) . ' ,\'"]\') matchcols, ', $tsql);
+                    } else {
+                        $tsql = str_replace('{matchcols}', ' \'[]\' matchcols,', $tsql);
                     }
 
 
                     $sqls[] = $tsql;
                 }
-                $sql = implode(' union ',$sqls).' order by searchformula desc ,datum desc';
+                $sql = implode(' union ', $sqls) . ' order by searchformula desc ,datum desc';
 
 
 
@@ -132,14 +135,133 @@ class OpenReports implements IRoute{
                 A::result('data', $data);
                 A::result('total', count($data));
                 A::result('success', true);
-                
-            }catch(\Exception $e){
-        
-                A::result('last_sql', $db->last_sql );
+            } catch (\Exception $e) {
+
+                A::result('last_sql', $db->last_sql);
                 A::result('msg', $e->getMessage());
             }
             A::contenttype('application/json');
-        },array('get'),true);
+        }, array('get'), true);
 
+
+
+        Route::add('/fints/reports', function ($matches) {
+
+            $db = A::get('session')->getDB();
+            try {
+
+                $sql = 'select id,name,tabellenzusatz,adress_bezug,preis_bezug,bezug_id,bezug_kst from blg_config';
+                $belege = $db->direct($sql, [], 'id');
+
+                $sql = 'select id,name,lower(tabellenzusatz) tabellenzusatz,adress_bezug,preis_bezug,bw_faktor,bezug_id,bezug_kst from blg_config';
+                $belege_tz = $db->direct($sql, [], 'tabellenzusatz');
+
+
+                /*
+                $sql = 'select base_table,blg_table,name,id_column from bezug_config';
+                $bezug_config = $db->direct($sql,[],'base_table');
+                */
+
+                $sql = 'select table_name,displayfield,searchfield from ds';
+                $ds_config = $db->direct($sql, [], 'table_name');
+
+                $tzlist = ['rechnung', 'krechnung'];
+                // array('rechnung','krechnung');
+                $columns = $db->direct('select table_name,column_name from ds_column where  column_name<>"offen" ');
+
+                $sql_template = '
+                    SELECT
+                        \'{tz}\' tabellenzusatz,
+                        \'{belegartid}\' belegartid,
+                        {bw_faktor} bw_faktor,
+                        blg_hdr_{tz}.id,
+                        blg_hdr_{tz}.id belegnummer,
+                        concat(blg_hdr_{tz}.id,\' \',blg_hdr_{tz}.datum, if(blg_hdr_{tz}.referenz<>\'\', concat(\' \',blg_hdr_{tz}.referenz),\'\'), \' \', round( {bw_faktor}*blg_hdr_{tz}.offen,2) ) _display,
+                        blg_hdr_{tz}.datum,
+                        blg_hdr_{tz}.buchungsdatum,
+                        blg_hdr_{tz}.faelligam,
+                        {dstable}.{dsname} name,
+                        
+                        {searchformula}
+                        blg_hdr_{tz}.zahlart,
+                        blg_hdr_{tz}.referenz,
+                        concat(blg_{bez}_{tz}.kundennummer,\'-\',blg_{bez}_{tz}.kostenstelle) bezugsnummer,
+                        {bw_faktor}*blg_hdr_{tz}.netto netto,
+                        {bw_faktor}*blg_hdr_{tz}.brutto brutto,
+                        {bw_faktor}*blg_hdr_{tz}.offen offen
+                    FROM
+                        blg_hdr_{tz} join blg_{bez}_{tz}
+                            on blg_{bez}_{tz}.id = blg_hdr_{tz}.id
+                        join view_readtable_{dstable} {dstable}
+                            on {dstable}.{idcolumn} = blg_{bez}_{tz}.kundennummer
+                            {kst_concat}
+
+
+                ';
+                $formula = array();
+
+                if (isset($_REQUEST['query'])) {
+                    $words = explode(' ', $_REQUEST['query']);
+                    foreach ($words as $word) {
+                        $formula[] = ' if({dstable}.{searchfield} like \'%' . $word . '%\',1,0 ) ';
+                        $formula[] = ' if({dstable}.{idcolumn} like \'%' . $word . '%\',1,0 ) ';
+                        $formula[] = ' if(blg_hdr_{tz}.id like \'%' . $word . '%\',1,0 ) ';
+                        $formula[] = ' if(blg_hdr_{tz}.referenz like \'%' . $word . '%\',1,0 ) ';
+                    }
+                    $sql_template .= ' WHERE {searchformula_where} >= ' . count($words) . '  ';
+                }
+
+                $sqls = array();
+                foreach ($tzlist as $tz) {
+                    $beleg_index = $belege_tz[$tz]['id'];
+                    $bw_faktor = $belege_tz[$tz]['bw_faktor'];
+                    $tsql = $sql_template;
+
+                    if (count($formula) > 0) {
+                        $tsql = str_replace('{searchformula}', ' ' . implode('+', $formula) . ' searchformula, ', $tsql);
+                        $tsql = str_replace('{searchformula_where}', ' ' . implode('+', $formula) . '  ', $tsql);
+                    } else {
+                        $tsql = str_replace('{searchformula}', ' 0 searchformula,', $tsql);
+                        $tsql = str_replace('{searchformula_where}', ' 0 ', $tsql);
+                    }
+
+
+                    $tsql = str_replace('{kst_concat}', ' and {dstable}.kostenstelle = blg_{bez}_{tz}.kostenstelle ', $tsql);
+                    $tsql = str_replace('{tz}', $tz, $tsql);
+                    $tsql = str_replace('{bw_faktor}', $bw_faktor, $tsql);
+                    $tsql = str_replace('{belegartid}', $beleg_index, $tsql);
+
+                    $tsql = str_replace('{bez}', $belege_tz[$tz]['adress_bezug'], $tsql);
+                    $tsql = str_replace('{dstable}', $belege_tz[$tz]['adress_bezug'], $tsql);
+                    $tsql = str_replace('{idcolumn}', $belege_tz[$tz]['bezug_id'], $tsql);
+                    $tsql = str_replace('{dsname}', $ds_config[$belege_tz[$tz]['adress_bezug']]['displayfield'], $tsql);
+                    $tsql = str_replace('{searchfield}', $ds_config[$belege_tz[$tz]['adress_bezug']]['searchfield'], $tsql);
+
+
+                    $matchcols = self::columns_table_name_filter($columns, $belege_tz[$tz]['adress_bezug']);
+                    if (count($matchcols) > 0) {
+                        $tsql = str_replace('{matchcols}', ' concat( \'["\',' . implode(',\'","\',', $matchcols) . ' ,\'"]\') matchcols, ', $tsql);
+                    } else {
+                        $tsql = str_replace('{matchcols}', ' \'[]\' matchcols,', $tsql);
+                    }
+
+
+                    $sqls[] = $tsql;
+                }
+                $sql = implode(' union ', $sqls) . ' order by searchformula desc ,datum desc';
+
+
+
+                $data = $db->direct($sql);
+                A::result('data', $data);
+                A::result('total', count($data));
+                A::result('success', true);
+            } catch (\Exception $e) {
+
+                A::result('last_sql', $db->last_sql);
+                A::result('msg', $e->getMessage());
+            }
+            A::contenttype('application/json');
+        }, array('get'), true);
     }
 }
--- src/js/lazy/models/AccountViewForm.js
+++ src/js/lazy/models/AccountViewForm.js
@@ -1,135 +1,166 @@
 Ext.define('Tualo.FinTS.models.AccountViewForm', {
-    extend: 'Ext.app.ViewModel',
-    alias: 'viewmodel.fints_accountview_form',
-    requires: [
-        'Tualo.FinTS.models.TanModes'
-    ],
-    data:{
-      id: '',
-      betrag: 0,
-      valuta: new Date(),
-      name: ' ',
-      bezugsnummer:'',
-      belegnummer:'',
-      verwendungszweck:'',
-      svwz: '',
-      cred: '',
-      debt: '',
-      eref: '',
-      mref: '',
-      _ist_bezug: '',
-      _ist_beleg: [],
-      _force_beleg: [],
-      _ist_betrag: 0,
-      _ist_status: '',
-      _state: 0
-    },
-    formulas: {
-      canSave: function(get){
-        if (get('_ist_betrag')!=0){
-          return true;
-        }
-        if (get('_force_beleg').length!=0){
-          return true;
-        }
-  
-        return false;
-      },
-      datum_wert_id: function(get){
-        return Ext.util.Format.date(get('valuta'),'d.m.Y')+' / '+Ext.util.Format.number(get('betrag'),'0.000,00/i')+'€';
-      },
-      display_ist_betrag: function(get){
-        return Ext.util.Format.number(get('_ist_betrag'),'0.000,00/i')+'€';
-      },
-      display_status: function(get){
-        if (get('_ist_status')!=''){
-          return get('_ist_status');
-        }else{
-          if (get('betrag')==get('_ist_betrag')){
-            return 'Der Betrag stimmt überein.';
-          }
-          if (get('betrag')<get('_ist_betrag')){
-            return 'Der Kontobetrag ist kleiner als der offene Betrag des Beleges.';
-          }
-          if (get('betrag')>get('_ist_betrag')){
-            return 'Der Kontobetrag ist größer als der offene Betrag des Beleges.';
-          }
-        }
-        return '';
+  extend: 'Ext.app.ViewModel',
+  alias: 'viewmodel.fints_accountview_form',
+  requires: [
+    'Tualo.FinTS.models.TanModes'
+  ],
+  data: {
+    id: '',
+    betrag: 0,
+    valuta: new Date(),
+    name: ' ',
+    bezugsnummer: '',
+    belegnummer: '',
+    verwendungszweck: '',
+    svwz: '',
+    cred: '',
+    debt: '',
+    eref: '',
+    mref: '',
+    _ist_bezug: '',
+    _ist_beleg: [],
+    _force_beleg: [],
+    _ist_betrag: 0,
+    _ist_status: '',
+    _state: 0
+  },
+  formulas: {
+    canSave: function (get) {
+      if (get('_ist_betrag') != 0) {
+        return true;
       }
+      if (get('_force_beleg').length != 0) {
+        return true;
+      }
+
+      return false;
     },
-    stores: {
-        status: {
-            fields: [
-              {name: 'id', type: 'string'},
-              {name: 'name',  type: 'string'}
-            ],
-            listeners: {
-              load: 'onStatusStoreLoad'
-            },
-            pageSize: 5000,
-            proxy: {
-              type: 'ajax',
-              url: './fints/statusbuttons',
-              reader: {
-                type: 'json',
-                rootProperty: 'data',
-                idProperty: 'id'
-              }
-            },
-            autoLoad: false
-          },
-          open: {
-            fields: [
-              {name: 'id', type: 'string'},
-              {name: 'tabellenzusatz',  type: 'string'},
-              {name: 'belegartid',       type: 'number'},
-              {name: 'bw_faktor',       type: 'number'},
-              {name: '_display',       type: 'string'},
-      
-              {name: 'matchcols', type:'string'} ,
-              {name: 'datum',  type: 'date'},
-              {name: 'buchungsdatum',  type: 'date'},
-              {name: 'belegnummer',  type: 'string'},
-              {name: 'referenz',  type: 'string'},
-              {name: 'bezugnummer',  type: 'string'},
-              {name: 'name',  type: 'string'},
-              {name: 'brutto',  type: 'number'},
-              {name: 'offen',  type: 'number'}
-            ],
-            pageSize: 5000,
-            proxy: {
-              type: 'ajax',
-              url: './fints/openreports',
-              reader: {
-                type: 'json',
-                rootProperty: 'data',
-                idProperty: 'id'
-              }
-            },
-            autoLoad: true
-          },
-      
-      
-          bezug: {
-            fields: [
-              {name: 'id', type: 'string'},
-              {name: 'name',  type: 'string'},
-              {name: 'matchcols',  type: 'string'},
-              {name: 'offen',  type: 'number'}
-            ],
-            pageSize: 5000,
-            proxy: {
-              type: 'ajax',
-              url: './fints/relations',
-              
-              reader: {
-                type: 'json',
-                rootProperty: 'data',
-                idProperty: 'id'
-              }
-            },
-            autoLoad: true
-          },
+    datum_wert_id: function (get) {
+      return Ext.util.Format.date(get('valuta'), 'd.m.Y') + ' / ' + Ext.util.Format.number(get('betrag'), '0.000,00/i') + '€';
+    },
+    display_ist_betrag: function (get) {
+      return Ext.util.Format.number(get('_ist_betrag'), '0.000,00/i') + '€';
+    },
+    display_status: function (get) {
+      if (get('_ist_status') != '') {
+        return get('_ist_status');
+      } else {
+        if (get('betrag') == get('_ist_betrag')) {
+          return 'Der Betrag stimmt überein.';
+        }
+        if (get('betrag') < get('_ist_betrag')) {
+          return 'Der Kontobetrag ist kleiner als der offene Betrag des Beleges.';
+        }
+        if (get('betrag') > get('_ist_betrag')) {
+          return 'Der Kontobetrag ist größer als der offene Betrag des Beleges.';
+        }
+      }
+      return '';
     }
+  },
+  stores: {
+    status: {
+      fields: [
+        { name: 'id', type: 'string' },
+        { name: 'name', type: 'string' }
+      ],
+      listeners: {
+        load: 'onStatusStoreLoad'
+      },
+      pageSize: 5000,
+      proxy: {
+        type: 'ajax',
+        url: './fints/statusbuttons',
+        reader: {
+          type: 'json',
+          rootProperty: 'data',
+          idProperty: 'id'
+        }
+      },
+      autoLoad: false
+    },
+    open: {
+      fields: [
+        { name: 'id', type: 'string' },
+        { name: 'tabellenzusatz', type: 'string' },
+        { name: 'belegartid', type: 'number' },
+        { name: 'bw_faktor', type: 'number' },
+        { name: '_display', type: 'string' },
+
+        { name: 'matchcols', type: 'string' },
+        { name: 'datum', type: 'date' },
+        { name: 'buchungsdatum', type: 'date' },
+        { name: 'belegnummer', type: 'string' },
+        { name: 'referenz', type: 'string' },
+        { name: 'bezugnummer', type: 'string' },
+        { name: 'name', type: 'string' },
+        { name: 'brutto', type: 'number' },
+        { name: 'offen', type: 'number' }
+      ],
+      pageSize: 5000,
+      proxy: {
+        type: 'ajax',
+        url: './fints/openreports',
+        reader: {
+          type: 'json',
+          rootProperty: 'data',
+          idProperty: 'id'
+        }
+      },
+      autoLoad: true
+    },
+
+
+    all: {
+      fields: [
+        { name: 'id', type: 'string' },
+        { name: 'tabellenzusatz', type: 'string' },
+        { name: 'belegartid', type: 'number' },
+        { name: 'bw_faktor', type: 'number' },
+        { name: '_display', type: 'string' },
+
+        { name: 'matchcols', type: 'string' },
+        { name: 'datum', type: 'date' },
+        { name: 'buchungsdatum', type: 'date' },
+        { name: 'belegnummer', type: 'string' },
+        { name: 'referenz', type: 'string' },
+        { name: 'bezugnummer', type: 'string' },
+        { name: 'name', type: 'string' },
+        { name: 'brutto', type: 'number' },
+        { name: 'offen', type: 'number' }
+      ],
+      pageSize: 5000,
+      proxy: {
+        type: 'ajax',
+        url: './fints/reports',
+        reader: {
+          type: 'json',
+          rootProperty: 'data',
+          idProperty: 'id'
+        }
+      },
+      autoLoad: true
+    },
+
+    bezug: {
+      fields: [
+        { name: 'id', type: 'string' },
+        { name: 'name', type: 'string' },
+        { name: 'matchcols', type: 'string' },
+        { name: 'offen', type: 'number' }
+      ],
+      pageSize: 5000,
+      proxy: {
+        type: 'ajax',
+        url: './fints/relations',
+
+        reader: {
+          type: 'json',
+          rootProperty: 'data',
+          idProperty: 'id'
+        }
+      },
+      autoLoad: true
+    },
+  }
 });
